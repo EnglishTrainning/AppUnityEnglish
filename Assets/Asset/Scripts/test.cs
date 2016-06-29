8b3136c9552f5c940ed352875e60482613d28b74
mac <hieuluu@innoria.com>
2016-06-28 18:52:47 -0700

    test

---
+++ Assets/Asset/Scripts/test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+public class test : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		ParseObject gameScore = new ParseObject("GameScore");
+		gameScore["score"] = 1337;
+		gameScore["playerName"] = "HieuLuu";
+		Task saveTask = gameScore.SaveAsync();
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
